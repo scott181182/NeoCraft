584c5630b6d5f29f046eed88298765f99d6462ff
Froz3nPho3nix <nolanrosen15@gmail.com>
2013-06-03 22:20:09 -0400

    Update 1

--- NeoCraft_common/MMC/neocraft/block/KilnBakery.java
+++ NeoCraft_common/MMC/neocraft/block/KilnBakery.java
@@ -1,11 +1,15 @@
 package MMC.neocraft.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
-public class KilnBakery extends NCblock
+public class KilnBakery extends NCcontainerBlock
 {
 	public KilnBakery(int par1, Material material)
 	{
 		super(par1, material);
 	}
+
+	@Override public TileEntity createNewTileEntity(World world) {return new TileEntityKilnBakery();}
 }
---
+++ NeoCraft_common/MMC/neocraft/tileentity/TileEntityKilnBakery.java
@@ -0,0 +1,99 @@
+package MMC.neocraft.tileentity;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+public class TileEntityKilnBakery extends NCtileentity 
+{
+
+	@Override
+	public int[] getAccessibleSlotsFromSide(int var1)
+	{
+		return null;
+	}
+
+	@Override
+	public boolean canInsertItem(int i, ItemStack itemstack, int j)
+	{
+		return false;
+	}
+
+	@Override
+	public boolean canExtractItem(int i, ItemStack itemstack, int j) 
+	{
+		return false;
+	}
+
+	@Override
+	public int getSizeInventory() 
+	{
+		return 0;
+	}
+
+	@Override
+	public ItemStack getStackInSlot(int i) 
+	{
+		return null;
+	}
+
+	@Override
+	public ItemStack decrStackSize(int i, int j) 
+	{
+		return null;
+	}
+
+	@Override
+	public ItemStack getStackInSlotOnClosing(int i) 
+	{
+		return null;
+	}
+
+	@Override
+	public void setInventorySlotContents(int i, ItemStack itemstack) 
+	{
+		
+	}
+
+	@Override
+	public String getInvName() 
+	{
+		return null;
+	}
+
+	@Override
+	public boolean isInvNameLocalized() 
+	{
+		return false;
+	}
+
+	@Override
+	public int getInventoryStackLimit() 
+	{
+		return 0;
+	}
+
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer entityplayer)
+	{
+		return false;
+	}
+
+	@Override
+	public void openChest() 
+	{
+		
+	}
+
+	@Override
+	public void closeChest() 
+	{
+		
+	}
+
+	@Override
+	public boolean isStackValidForSlot(int i, ItemStack itemstack) 
+	{
+		return false;
+	}
+
+}
